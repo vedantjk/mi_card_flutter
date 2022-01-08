1304c7b1ac022425a3b4ab22d28ba9224fd92c86
vedantjk <vedantkodagi2804@gmail.com>
2022-01-08 14:59:23 +0530

    Basic Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,120 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/i_am_rich_app_icon.png'),
+              ),
+              Text(
+                'Vedant Kodagi',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 9307546989',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Montserrat',
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'vedantkodagi2804@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Montserrat',
+                          fontSize: 15.0,
+                        ),
+                      )),
+                ),
+              ),
+            ],
+          ),
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
+
+// Row(
+// children: [
+// Icon(
+// Icons.email,
+// color: Colors.teal,
+// ),
+// SizedBox(
+// width: 10.0,
+// ),
+// Text(
+// 'vedantkodagi2804@gmail.com',
+// style: TextStyle(
+// color: Colors.teal.shade900,
+// fontFamily: 'Montserrat',
+// fontSize: 18.0,
+// ),
+// )
+// ],
+// ),
